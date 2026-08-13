a6d71391a44588ac6151c96d05b6eddd684707ea
heiyan2007 <gooslf90@gmail.com>
2026-08-13 17:10:25 +0800

    Add vite.pages.config.ts

---
+++ vite.pages.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "/hoi4-division-designer/",
+  plugins: [react()],
+  build: { outDir: "pages-dist", emptyOutDir: true },
+});
+
